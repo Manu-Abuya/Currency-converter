6348548093d7b1bb316691472ddf2814ed65f0ae
Manu-Abuya <manuabuya69@gmail.com>
2023-02-02 14:24:40 +0300

    Update

--- currency.py
+++ currency.py
@@ -19,3 +19,38 @@ def get_exchange_rate(from_currency, to_currency):
             return "Error: to_currency not found in exchange rates data."
     else: 
         return "Error: from_currency not found in exchange rates data."
+    
+
+def convert_currency(amount, from_currency, to_currency):
+    exchange_rate = get_exchange_rate(from_currency, to_currency)
+    
+    # Check if the exchange rate is valid
+    if type(exchange_rate) == float:
+        # Calculate the converted amount
+        converted_amount = exchange_rate * amount
+        return converted_amount
+    else:
+        return exchange_rate
+    
+
+# Main function to run the currency converter
+def run_currency_converter():
+    
+    # Get the amount to be converted
+    amount = float(input("Enter the amount to be converted: "))
+    
+    # Get the currency to convert from
+    from_currency = input("Enter the currency to convert from (e.g. USD): ").upper()
+  
+    # Get the currency to convert to
+    to_currency = input("Enter the currency to convert to (e.g. EUR): ").upper()
+   
+    # Convert the currency
+    converted_amount = convert_currency(amount, from_currency, to_currency)
+   
+    # Print the result
+    print(f"{amount} {from_currency} is equal to {converted_amount} {to_currency}")
+    
+    
+# Run the currency converter
+run_currency_converter()
